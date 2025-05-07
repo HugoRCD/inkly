4b8d56ebbe915ca0cc0c05301ff9c681890f7979
HugoRCD <hugo.richard@epitech.eu>
2025-05-07 21:04:45 +0200

    up

--- app/app.vue
+++ app/app.vue
@@ -21,6 +21,37 @@ useSeoMeta({
   ogTitle: 'Inkly - Email Signature Generator',
   ogDescription: 'The most simple open-source email signature generator',
 })
+
+useHead({
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    },
+  ],
+})
 </script>
 
 <template>
